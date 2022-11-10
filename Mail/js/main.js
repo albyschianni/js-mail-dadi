ab61f30d13c11d9736c6fa564b5579d988cd59fb
albyschianni <alberto.schianni@gmail.com>
2022-11-10 15:45:57 +0100

    final

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -14,22 +14,24 @@ myButton.addEventListener("click",
     function (){
         let myEmail = document.getElementById("mia-email").value;
         console.log(myEmail);
+
+        for (let i = 0; i < lista.length; i++){
+   
+            if (myEmail == lista [i]){
+                alert("sei presente")
+                var presente = 1;
+            }
+        }
+        
+        if (presente !== 1){
+            alert("Non risulti nella lista")
+        }
     }
     
 )
 
 // controllo se l'email inserita (valore?) sia presente all'interno della lista 
 
-for (let i = 0; i < lista.length; i++){
-   
-    if (myEmail == lista [i]){
-        alert("sei presente")
-        var presente = 1;
-    }
-}
 
-if (presente !== 1){
-    alert("Non risulti nella lista")
-}
 
 // output
